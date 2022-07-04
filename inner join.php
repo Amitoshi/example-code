0596447e60a2fe23995207abd77020950a00aa43
Amitoshi <71155696+Amitoshi@users.noreply.github.com>
2022-07-05 00:32:16 +0530

    Add files via upload

---
+++ inner join.php
@@ -0,0 +1,102 @@
+<table>
+<?php 
+
+$db = new MySQLi("localhost","root","","join");
+
+$sql = "SELECT Cname,Cityname,Sname 
+		FROM country t1 
+		INNER JOIN state t2 
+		ON t1.Cid = t2.Cid 
+		INNER JOIN city t3 
+		ON t2.Sid = t3.Sid 
+		";
+$res = $db->query($sql);
+while($row = $res->fetch_array()){
+	echo "<br>";
+	echo $row['Cname']." || ";
+	echo $row['Sname']." || ";
+	echo $row['Cityname'];
+	echo "<br>";
+
+}
+
+?>
+</table>
+<hr>
+
+<table>
+<?php 
+
+$db = new MySQLi("localhost","root","","join");
+
+$sql = "SELECT COUNT(*) AS count
+		FROM state WHERE Cid = '3' 
+		 
+		";
+$res = $db->query($sql);
+while($row = $res->fetch_array()){
+	echo "<br>";
+
+	echo $row['count']." || ";
+
+	echo "<br>";
+
+}
+
+?>
+</table>
+
+
+<hr>
+
+<table>
+<?php 
+
+$db = new MySQLi("localhost","root","","join");
+
+$sql = "SELECT *, COUNT(*) AS count FROM state GROUP BY (Cid) HAVING COUNT(*) >1 
+		 
+		";
+$res = $db->query($sql);
+while($row = $res->fetch_array()){
+	echo "<br>";
+
+	echo $row['Sid']." || ";
+	echo $row['Sname']." || ";
+	echo $row['Cid']." || ";
+
+	echo "<br>";
+
+}
+
+?>
+</table>
+
+
+
+<hr>
+<!-- intersect -->
+
+<table>
+<?php 
+
+$db = new MySQLi("localhost","root","","join");
+
+$sql2 = "SELECT Cityname AS Ct  FROM city  UNION DISTINCT  SELECT Sname AS Sn FROM state  ";
+$res2 = $db->query($sql2);
+
+while($row2 = $res2->fetch_array()){
+	echo "<br>";
+
+	
+
+	echo $row2['Ct']." || ";
+	// echo $row2['Sn']." || ";
+
+	echo "<br>";
+
+}
+
+?>
+</table>
+
